21239b97a7538c290af3ef4c965176939e2ac442
Marc Choisy <marc.choisy@ird.fr>
2019-04-16 11:46:35 +0700

    Adding a script to read the whole CliRes database

---
+++ reading_clires.R
@@ -0,0 +1,23 @@
+library(readxl)
+library(magrittr)
+
+data_folder <- "../data from CliRes"
+files <- dir(data_folder, full.names = TRUE)
+
+read_excel_file <- function(x) {
+  require(readxl)
+  sheet_names <- excel_sheets(x)
+  setNames(lapply(sheet_names, read_xls, path = x), sheet_names)
+}
+
+read_data <- function(x) {
+  require(magrittr)
+  files %>%
+    grep("Category", ., value = TRUE, invert = x) %>%
+    read_excel_file()
+}
+
+drugs <- read_data(FALSE)
+surveillance <- read_data(TRUE)
+
+
