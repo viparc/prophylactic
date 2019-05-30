26cdd0bc3efe76b7392d7262e70c984c4d9a558f
Marc Choisy <marc.choisy@ird.fr>
2019-05-30 17:03:25 +0700

    Updating make_data.Rmd

--- make_data.R
+++ make_data.R
@@ -1,66 +1,98 @@
 library(magrittr)
 
-path <- "../data from CliRes/28-3-2019-_18ZN_"
+clires_folder <- "../data from CliRes/16-4-2019-_18ZN_"
 antimicrobial <- c("ANTIMICROBIAL", "ANTIMICROBIAL_GridEvent")
 disease <- c("DISEASE", "DISEASE_GridEvent")
+chicken <- c("MID_INOUT", "MID_INOUT_GridEvent")
 
 lapply2 <- function(X, FUN, ...) setNames(lapply(X, FUN, ...), X)
 
-surveillance <- c(antimicrobial, disease) %>%
-  lapply2(readxl::read_excel, path = paste0(path, "V1_Data.xls"))
+esvac <- read.csv("data/esvac.csv") %>% 
+  dplyr::filter(!grepl("methane", ab_vet_med)) %>% 
+  dplyr::select(ab_oie, mg.IU) %>% 
+  dplyr::mutate(ab_oie = sub(" .*$", "", tolower(ab_oie))) %>% 
+  dplyr::bind_rows(data.frame(ab_oie = "josamycin",
+                              mg.IU  = 0.001, stringsAsFactors = FALSE)) %$%
+  setNames(mg.IU, ab_oie)
 
+surveillance <- c(antimicrobial, disease, chicken, "SAMPLE") %>%
+  lapply2(readxl::read_excel, path = paste0(clires_folder, "V1_Data.xls"))
+
+samplesdata <- surveillance$SAMPLE %>% 
+  dplyr::select(USUBJID, FLOCKSEQUENCE, SAMPLINGDATE, SAMPLINGVISIT)
+
+samplesdata %>%
+  dplyr::filter(SAMPLINGVISIT == "S") %>%
+  select(-SAMPLINGVISIT) %>%
+  rename(start = SAMPLINGDATE) %>%
+  right_join(samplesdata) %>%
+  mutate(WEEK     = as.integer(floor((SAMPLINGDATE - start) / (60 * 60 * 24 * 7)) + 1),
+         sampling = TRUE) %>% 
+  select(-start, -SAMPLINGDATE, -SAMPLINGVISIT)
+  
+chickdata <- surveillance[chicken] %>% 
+  unname() %>% 
+  purrr::invoke(dplyr::left_join, ., by = c("USUBJID", "MID_INOUT_SEQ")) %>% 
+  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEK, CHICKENTOTAL) %>% 
+  dplyr::mutate_at("CHICKENTOTAL", as.integer)
+  
 amu0 <- surveillance[antimicrobial] %>% 
   unname() %>% 
   purrr::invoke(dplyr::left_join, ., by = c("USUBJID", "ANTIMICROBIAL_SEQ")) %>% 
-  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEKNO, CODE) %>% 
-  na.exclude()
+  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEKNO, CODE, AMOUTUSED, PACKAGEUNIT) %>%
+  na.exclude() %>% 
+  dplyr::mutate(PACKAGEUNIT = dplyr::na_if(PACKAGEUNIT, "TAB"))
 
 drug_data <- c("ANTIMICROBIAL", "ANTIMICROBIAL_GridAnti") %>% 
-  lapply(readxl::read_excel, path = paste0(path, "Category_V1_Data.xls"))
-
-dictionary <- drug_data[[2]] %>%
-  dplyr::select(ANTINAME, ANTINAME1) %>%
-  na.exclude() %>%
-  unique() %>% 
-  dplyr::filter(!(ANTINAME == "NORFLOXACIN"      & ANTINAME1 == "neomycin")) %>% 
-  dplyr::filter(!(ANTINAME == "SUNFAMETHOXAZOLE" & ANTINAME1 == "sunphamethoxazole")) %$%
-  setNames(ANTINAME1, ANTINAME) %>% 
-  c("ERYTHROMYCIN THIOCYANATE" = "erythromycin",
-    "APRAMYCIN SULFATE"        = "apramycin",
-    "CEFTIOFUR (HCL)"          = "ceftiofur",
-    "SULFADIAZINE SODIUM"      = "sulfadiazine",
-    "Florfenicol"              = "florfenicol",
-    "Tylosin"                  = "tylosin",
-    "Marbofloxacin"            = "marbofloxalin",
-    "GENTAMYCIN SULFAT"        = "gentamicin",
-    "COLISTIN (SULFATE)"       = "colistin")
+  lapply(readxl::read_excel, path = paste0(clires_folder, "Category_V1_Data.xls"))
 
 drug_codes <- drug_data %>% 
   purrr::invoke(dplyr::right_join, ., by = "ANTIMICROBIAL_SEQ") %>% 
-  dplyr::select(CODE, ANTINAME) %>%
-  dplyr::mutate(ANTINAME = dictionary[ANTINAME]) %>% 
-  unique() %>% 
-  dplyr::filter(! ANTINAME %in% c("alicin", "axit oxolinic", "iodo-hydroxyquinoline", "metronidazol", "nystatin")) %>% 
-  dplyr::mutate(ANTINAME = dplyr::recode(ANTINAME, sunfadimethoxine  = "sulfadimethoxine"))
+  dplyr::select(CODE, ANTINAME1, CONCENTRATION, GRIDUNIT, GRIDPACK, GRIDPACKUNIT) %>%
+  dplyr::filter(! ANTINAME1 %in% c("alicin", "axit oxolinic", "iodo-hydroxyquinoline", "metronidazol", "nystatin")) %>% 
+  dplyr::mutate(ANTINAME1     = dplyr::recode(ANTINAME1, sunfadimethoxine  = "sulfadimethoxine",
+                                                         sunphamethoxazole = "sulphamethoxazole"),
+                GRIDUNIT      = dplyr::na_if(GRIDUNIT, "NA"),
+                GRIDUNIT      = dplyr::na_if(GRIDUNIT, "na"),
+                GRIDUNIT      = dplyr::recode(GRIDUNIT, UI = "IU", mg = "MG"),
+                GRIDPACKUNIT  = dplyr::na_if(GRIDPACKUNIT, "na"),
+                GRIDPACKUNIT  = dplyr::na_if(GRIDPACKUNIT, "VI"),
+                GRIDPACKUNIT  = dplyr::recode(GRIDPACKUNIT, g  = "G", ml = "G", ML = "G"),
+                CONCENTRATION = dplyr::na_if(CONCENTRATION, "NA"),
+                CONCENTRATION = dplyr::na_if(CONCENTRATION, "na"),
+                CONCENTRATION = dplyr::recode(CONCENTRATION, S = "500"), # this will ultimately be corrected in CliRes
+                CONCENTRATION = as.numeric(CONCENTRATION),
+                CONCENTRATION = ifelse(GRIDUNIT == "IU", CONCENTRATION * esvac[ANTINAME1], CONCENTRATION),
+                GRIDUNIT      = ifelse(GRIDUNIT == "IU", "MG", GRIDUNIT), # HAS TO BE HERE!!!
+                CONCENTRATION = ifelse(GRIDUNIT == "MG", CONCENTRATION / 1000, CONCENTRATION),
+                GRIDUNIT      = ifelse(GRIDUNIT == "MG", "G", GRIDUNIT), # HAS TO BE HERE!!!
+                GRIDPACK      = dplyr::na_if(GRIDPACK, "na"),
+                GRIDPACK      = as.numeric(GRIDPACK),
+                GRIDPACK      = ifelse(GRIDPACKUNIT == "KG", 1000 * GRIDPACK, GRIDPACK),
+                GRIDPACKUNIT  = ifelse(GRIDPACKUNIT == "KG", "G", GRIDPACKUNIT), # HAS TO BE HERE!!! 
+                proportion    = CONCENTRATION / GRIDPACK) %>% 
+  unique()
 
 amu <- dplyr::left_join(amu0, drug_codes, "CODE") %>%
-  dplyr::mutate(presence = 1) %>%
+  tidyr::replace_na(list(ANTINAME1 = "unknown")) %>% # because some antibiotic names are unknown
+  dplyr::mutate(presence   = 1,
+                antibiotic = proportion * AMOUTUSED,
+                antiname   = toupper(ANTINAME1)) %>%
+  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEKNO, ANTINAME1, presence, antibiotic, antiname) %>% 
   tibble::rowid_to_column() %>% # spread require unique row identifiers
-  tidyr::replace_na(list(ANTINAME = "unknown")) %>% # because some antibiotic names are unknown
-  tidyr::spread(ANTINAME, presence) %>%
-  dplyr::select(-CODE, -rowid) %>% # we don't need CODE and rowid anymore
+  tidyr::spread(ANTINAME1, presence) %>%
+  tidyr::spread(antiname, antibiotic) %>%
+  dplyr::select(-rowid) %>% # we don't need rowid anymore
   replace(is.na(.), 0) %>%
   dplyr::group_by(USUBJID, FLOCKSEQUENCE, WEEKNO) %>%
   dplyr::summarise_all(~sum(.)) %>%
-  dplyr::mutate_at(dplyr::vars(-USUBJID, -FLOCKSEQUENCE, -WEEKNO), function(x) x > 0)
+  dplyr::mutate_at(vars(matches("^[a-z]", FALSE)), function(x) x > 0)
 
 symptoms <- surveillance[disease] %>% 
   unname() %>% 
   purrr::invoke(dplyr::left_join, ., by = c("USUBJID", "DISEASE_SEQ")) %>%
-  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEK, RESPIRATORY, DIARRHOEA, CNS, MALAISE, LEGLESIONS, SUDDENDEATH)
-
-# corrections on weeks:
-symptoms %<>%
+  dplyr::select(USUBJID, FLOCKSEQUENCE, WEEK, RESPIRATORY, DIARRHOEA, CNS, MALAISE, LEGLESIONS, SUDDENDEATH) %>% 
+# corrections on weeks (this cannot be corrected in CliRes and has to be corrected here)
 # correction 1: 2 weeks 8 and no week 9 for cycle 2 of farm 8. These 2 weeks are identical
   dplyr::filter(USUBJID == "75-008", FLOCKSEQUENCE == "02", WEEK == 8) %>% 
   dplyr::mutate(WEEK = 8:9) %>%
@@ -79,9 +111,14 @@ symptoms %>%
 # Checking that there are no week defined in amu but not in symptoms:
 dplyr::anti_join(amu, symptoms, by = c("USUBJID", "FLOCKSEQUENCE", "WEEKNO" = "WEEK"))
 
+# Checking that there are no week defined in chickdata but not in viparc:
+dplyr::anti_join(chickdata, symptoms, by = c("USUBJID", "FLOCKSEQUENCE", "WEEK"))
+
 # Merging:
-viparc <- dplyr::left_join(symptoms, amu, by = c("USUBJID", "FLOCKSEQUENCE", "WEEK" = "WEEKNO")) %>% 
-  replace(is.na(.), FALSE) %>%
+viparc <- symptoms %>% 
+  dplyr::left_join(amu, by = c("USUBJID", "FLOCKSEQUENCE", "WEEK" = "WEEKNO")) %>% 
+  dplyr::left_join(chickdata, by = c("USUBJID", "FLOCKSEQUENCE", "WEEK")) %>% 
+  replace(is.na(.), FALSE) %>% ### what is this for???
   dplyr::mutate_at(dplyr::vars(FLOCKSEQUENCE, WEEK), as.integer) %>% 
   dplyr::arrange(USUBJID, FLOCKSEQUENCE, WEEK)
 
