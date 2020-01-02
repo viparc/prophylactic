c5b73847335073ac844d54714156d32f53c85f9e
Marc Choisy <marc.choisy@ird.fr>
2020-01-02 17:30:33 +0700

    Moving scripts to clires data

--- making_weight_data.R
+++
@@ -1,8 +0,0 @@
-if (!dir.exists("data")) dir.create("data")
-"/Users/choisy/Dropbox/aaa/projects/ViParc/AMU metrics/Supplementary_Data_Frontiers.xlsx" %>% 
-  readxl::read_excel() %>% 
-  tidyr::gather("week", "weight", -FlockID, -Chicken) %>% 
-  na.exclude() %>% 
-  dplyr::mutate(week = sub("Week ", "", week)) %>% 
-  dplyr::mutate_at(c("FlockID", "Chicken", "week"), as.integer) %>% 
-  write.csv("data/weights.csv", FALSE, row.names = FALSE)
